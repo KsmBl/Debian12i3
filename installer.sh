3bc22af28f241a001d4376b9203abdb65a432576
KsmBl <katzen.sind.lecker69@gmail.com>
2025-01-17 15:34:05 +0000

    add configs

--- installer.sh
+++ installer.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-echo "test"
+sudo apt install fish htop neofetch vlc i3 polybar picom numlockx playerctl rofi thunderbird vim
+sudo wget "https://config.whisper-me.de/i3/config" -P /etc/polybar/
+sudo wget "https://config.whisper-me.de/picom/picom.conf" -P ~/.config/picom/
+sudo wget "https://config.whisper-me.de/fish/config.fish" -P ~/.config/fish/
+sudo wget "https://config.whisper-me.de/htop/htoprc" -P ~/.config/htop/
+sudo wget "https://config.whisper-me.de/neofetch/config.conf" -P ~/.config/neofetch/
+sudo wget "https://config.whisper-me.de/rofi/config.rasi" -P ~/.config/rofi/
+sudo wget "https://config.whisper-me.de/i3/config" -P ~/.config/i3/
+sudo wget "https://config.whisper-me.de/vim/.vimrc" -P ~/
