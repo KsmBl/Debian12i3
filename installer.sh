c87984667cb03fdff83aa2e3422016ef07b1c4f7
KsmBl <katzen.sind.lecker69@gmail.com>
2025-01-17 15:36:03 +0000

    add wget to installs

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo apt install fish htop neofetch vlc i3 polybar picom numlockx playerctl rofi thunderbird vim
+sudo apt install fish htop neofetch vlc i3 polybar picom numlockx playerctl rofi thunderbird vim wget
 sudo wget "https://config.whisper-me.de/i3/config" -P /etc/polybar/
 sudo wget "https://config.whisper-me.de/picom/picom.conf" -P ~/.config/picom/
 sudo wget "https://config.whisper-me.de/fish/config.fish" -P ~/.config/fish/
