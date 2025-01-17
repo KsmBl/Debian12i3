6c3501f879eb6b612ba2fad9aa0891c640db0002
KsmBl <katzen.sind.lecker69@gmail.com>
2025-01-17 15:42:16 +0000

    set chown for config files

--- installer.sh
+++ installer.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 sudo apt install fish htop neofetch vlc i3 polybar picom numlockx playerctl rofi thunderbird vim wget
 sudo wget "https://config.whisper-me.de/i3/config" -P /etc/polybar/
+sudo chown "$USER:$USER" /etc/polybar
+
 sudo wget "https://config.whisper-me.de/picom/picom.conf" -P ~/.config/picom/
 sudo wget "https://config.whisper-me.de/fish/config.fish" -P ~/.config/fish/
 sudo wget "https://config.whisper-me.de/htop/htoprc" -P ~/.config/htop/
 sudo wget "https://config.whisper-me.de/neofetch/config.conf" -P ~/.config/neofetch/
 sudo wget "https://config.whisper-me.de/rofi/config.rasi" -P ~/.config/rofi/
 sudo wget "https://config.whisper-me.de/i3/config" -P ~/.config/i3/
+sudo chown "$USER:$USER" ~/.config -R
+
 sudo wget "https://config.whisper-me.de/vim/.vimrc" -P ~/
+sudo chown "$USER:$USER" ~/.vimrc
 
 chsh -s /usr/bin/fish
